2bf1945f385fd1bcf5586c1178f69f5ac2c10703
stanleyalv3s <comercial@rioinforma.com.br>
2017-08-29 16:37:42 -0300

    Criando Validações

---
+++ src/ConsoleTesting/ConsoleTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using Eventos.IO.Domain.Models;
+using System;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var evento = new Evento(
+                "No",
+                DateTime.Now,
+                DateTime.Now,
+                true, 
+                50,
+                false,
+                "Stanley Alves");
+
+            var evento2 = evento;
+
+            Console.WriteLine(evento.ToString());
+
+            if(!evento.EhValido())
+            {
+                foreach (var erro in evento.ErrosValidacao)
+                {
+                    Console.WriteLine(erro.Value);                           
+                }
+            }
+
+            //Console.WriteLine(evento.Equals(evento2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Eventos.IO.Domain/Models/Evento.cs
@@ -0,0 +1,66 @@
+﻿using Eventos.IO.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eventos.IO.Domain.Models
+{
+    public class Evento : Entity
+    {
+        public Evento(string nome, 
+                      DateTime dataInicio,
+                      DateTime datafim, 
+                      bool gratuito,
+                      decimal valor ,
+                      bool online, 
+                      string nomeEmpresa)
+       
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            DataInicio = dataInicio;
+            DataFim = datafim;
+            Gratuito = gratuito;
+            Valor = valor;
+            OnLine = online;
+            NomeEmpresa = nomeEmpresa;
+
+
+            ErrosValidacao = new Dictionary<string, string>();
+
+            if (nome.Length < 3)
+                ErrosValidacao.Add("Nome","O nome do evento deve ter mais de 3 caracteres");
+
+            if (gratuito && valor != 0)
+                ErrosValidacao.Add("Valor", "Não pode ter valor se gratuito");
+
+        }
+
+
+        public string Nome { get; private set; }
+        public string DescricaoCurta { get; private set; }
+        public string DescricaoLonga { get; private set; }
+        public DateTime DataInicio { get; private set; }
+        public DateTime DataFim { get; private set; }
+        public bool Gratuito { get; private set; }
+        public decimal Valor { get; private set; }
+        public bool OnLine { get; private set; }
+        public string NomeEmpresa { get; private set; }
+        public Categoria Categoria { get; private set; }
+        public ICollection<Tags> Tags { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public Organizador Organizador { get; private set; }
+
+        public Dictionary<string, string> ErrosValidacao { get; set; }
+
+        public bool EhValido()
+        {
+            return !ErrosValidacao.Any();
+        }
+    }
+
+
+    
+
+}
